27033063e18986f73d5359eaf20ab00c28dbc30e
Hidinkyu <Roysbae@gmail.com>
2022-10-13 19:31:59 -0700

    getting the start rolling

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+/*
+START button
+if value = true (started), do 3 things
+1. change START button text to STOP
+2. start Work Time timer
+*/
+
+/* Work time
+
+*/
+
+
+
+
+
+
+
+
+/* store user data  https://developer.chrome.com/docs/extensions/reference/storage/
+
+data stored
+1. goals - active and archived goals
+2. microtasks/microwins for each goal
+3. number of pomodoros for each goal
+*/
